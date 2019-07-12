c22e23d32dee0b895235a18a882b89779fb60a0f
Dan Naeem <dan.naeem95@gmail.com>
2019-07-12 13:27:22 -0400

    added routes for the 4 main components (to be created)

--- src/App.js
+++ src/App.js
@@ -5,6 +5,13 @@ import "boostrap/dist/css/bootstrap.min.css"
 function App() {
   return (
     <Router>
+      <Navbar />
+      <br />
+
+      <Route path='/' exact component={ExercisesList} />
+      <Route path='/edit/:id' component={EditExercise} />
+      <Route path='/create' component={CreateExercise} />
+      <Route path='/user' component={CreateUser} />
     </Router>
   );
 }
