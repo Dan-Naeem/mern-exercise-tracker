ef4a42556a308aee764ca1b97c13090bdcaa6fce
Dan Naeem <dan.naeem95@gmail.com>
2019-07-12 13:45:52 -0400

    fixed typo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Browser as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import "boostrap/dist/css/bootstrap.min.css"
 
 import Navbar from "./components/Navbar.component.js";
