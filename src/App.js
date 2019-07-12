6303d5caaca56634097572827fe0f7e2cb759039
Dan Naeem <dan.naeem95@gmail.com>
2019-07-12 13:34:12 -0400

    import components (to be created)

--- src/App.js
+++ src/App.js
@@ -2,6 +2,12 @@ import React from 'react';
 import { Browser as Router, Route } from "react-router-dom";
 import "boostrap/dist/css/bootstrap.min.css"
 
+import Navbar from "./components/Navbar.component.js";
+import ExercisesList from "./components/ExercisesList.component.js";
+import EditExercise from "./components/EditExercise.component.js";
+import CreateExercise from "./components/CreateExercise.component.js";
+import CreateUser from "./components/CreateUser.component.js";
+
 function App() {
   return (
     <Router>
